76b690c00f6faf6b4a02d36b90e015c3374c450b
Allan de Miranda Silva <allandemiranda@gmail.com>
2021-03-18 19:38:08 -0300

    mongo memory criado

--- src/services/mongoDB.service.js
+++ src/services/mongoDB.service.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-mongoose.connect(process.env.MONGO_HOST, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+const mongoServer = new MongoMemoryServer();
 
-mongoose.connection
-  .once('open', function () {
-    console.log('MongoDB database connection established successfully');
-    console.log(process.env.MONGO_HOST);
-  })
-  .on('error', function (error) {
-    console.log('Mongo Error is: ', error);
+mongoose.Promise = Promise;
+mongoServer.getUri().then(mongoUri => {
+  const mongooseOpts = {
+    // options for mongoose 4.11.3 and above
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+    useMongoClient: true, // remove this line if you use mongoose 5 and above
+  };
+
+  mongoose.connect(mongoUri, mongooseOpts);
+
+  mongoose.connection.on('error', e => {
+    if (e.message.code === 'ETIMEDOUT') {
+      console.log(e);
+      mongoose.connect(mongoUri, mongooseOpts);
+    }
+    console.log(e);
   });
+
+  mongoose.connection.once('open', () => {
+    console.log(`MongoDB successfully connected to ${mongoUri}`);
+  });
+});
 
 module.exports = mongoose;
