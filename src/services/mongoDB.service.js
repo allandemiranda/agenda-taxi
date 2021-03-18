d8e4590d95e4ff29e2c9bd6e122c91dda00029bb
Allan de Miranda Silva <allandemiranda@gmail.com>
2021-03-18 20:05:55 -0300

    fix mongo

--- src/services/mongoDB.service.js
+++ src/services/mongoDB.service.js
@@ -1,31 +1,50 @@
 const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
+if (process.env.TEST) {
+  const { MongoMemoryServer } = require('mongodb-memory-server');
 
-const mongoServer = new MongoMemoryServer();
+  const mongoServer = new MongoMemoryServer();
 
-mongoose.Promise = Promise;
-mongoServer.getUri().then(mongoUri => {
-  const mongooseOpts = {
-    // options for mongoose 4.11.3 and above
-    autoReconnect: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 1000,
-    useMongoClient: true, // remove this line if you use mongoose 5 and above
-  };
+  mongoose.Promise = Promise;
+  mongoServer.getUri().then(mongoUri => {
+    const mongooseOpts = {
+      // options for mongoose 4.11.3 and above
+      autoReconnect: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000,
+      useMongoClient: true, // remove this line if you use mongoose 5 and above
+    };
 
-  mongoose.connect(mongoUri, mongooseOpts);
+    mongoose.connect(mongoUri, mongooseOpts);
 
-  mongoose.connection.on('error', e => {
-    if (e.message.code === 'ETIMEDOUT') {
+    mongoose.connection.on('error', e => {
+      if (e.message.code === 'ETIMEDOUT') {
+        console.log(e);
+        mongoose.connect(mongoUri, mongooseOpts);
+      }
       console.log(e);
-      mongoose.connect(mongoUri, mongooseOpts);
-    }
-    console.log(e);
+    });
+
+    mongoose.connection.once('open', () => {
+      console.log(`MongoDB successfully connected to ${mongoUri}`);
+    });
+  });
+} else {
+  mongoose.connect(process.env.MONGO_HOST, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   });
 
-  mongoose.connection.once('open', () => {
-    console.log(`MongoDB successfully connected to ${mongoUri}`);
-  });
-});
+  mongoose.connection
+    .once('open', function () {
+      console.log(
+        `MongoDB successfully connected to ${process.env.MONGO_HOST}`,
+      );
+    })
+    .on('error', function (error) {
+      console.log('Mongo Error is: ', error);
+    });
+}
 
 module.exports = mongoose;
